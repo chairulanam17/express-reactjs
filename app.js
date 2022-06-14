824b5f148a6adada4696b159a48622d5b8fc24bd
chairulanam17 <chairulanam612@gmail.com>
2022-06-14 16:42:06 +0700

    set up local server

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-const userName = "Anam";
+// const fs = require("fs");
 
-console.log(userName);
+// const userName = "Anam";
+
+// fs.writeFile("user-data.txt", "Name: " + userName, (err) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log("wrote file");
+// });
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("incoming req");
+  console.log(req.method, req.url);
+
+  res.setHeader("Content-Type", "text/plain");
+  res.end("<h1>Sukses!</h1>");
+});
+
+server.listen(5001);
